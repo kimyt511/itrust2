04ec50dcad8799150b86a09d1dfaff6e20a97d49
Jimin Yoo <jmy0819_@naver.com>
2023-11-15 22:28:07 +0900

    refactor: Remove unused imports

--- src/main/java/edu/ncsu/csc/itrust2/models/PersonalRepresentative.java
+++ src/main/java/edu/ncsu/csc/itrust2/models/PersonalRepresentative.java
@@ -1,29 +1,14 @@
 package edu.ncsu.csc.itrust2.models;
 
-import edu.ncsu.csc.itrust2.adapters.ZonedDateTimeAdapter;
-import edu.ncsu.csc.itrust2.adapters.ZonedDateTimeAttributeConverter;
-import edu.ncsu.csc.itrust2.models.enums.AppointmentType;
-
-import java.time.ZonedDateTime;
-import java.util.List;
-import javax.persistence.Basic;
-import javax.persistence.CascadeType;
-import javax.persistence.Convert;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.google.gson.annotations.JsonAdapter;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
