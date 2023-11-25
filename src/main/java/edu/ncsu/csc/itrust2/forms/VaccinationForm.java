c9cec6fd88ed6fbb966e43828b2dc676de74ef3a
Chulwon Han <hchulwon3080@gmail.com>
2023-11-26 02:26:06 +0900

    Add: vaccination model

--- src/main/java/edu/ncsu/csc/itrust2/forms/VaccinationForm.java
+++ src/main/java/edu/ncsu/csc/itrust2/forms/VaccinationForm.java
@@ -1,4 +1,33 @@
 package edu.ncsu.csc.itrust2.forms;
 
-public class VaccinationForm {
+import edu.ncsu.csc.itrust2.models.Vaccination;
+import edu.ncsu.csc.itrust2.models.Vaccine;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Setter
+@NoArgsConstructor
+@Getter
+public class VaccinationForm implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String vaccine;
+    private String vaccinationDay;
+    private String patient;
+    private String comments;
+
+    public VaccinationForm(final Vaccination v){
+        this.id = v.getId();
+        this.vaccine = v.getVaccine().getName();
+        this.vaccinationDay = v.getVaccinationDay().toString();
+        this.patient = v.getPatient().getUsername();
+        this.comments = v.getComments();
+    }
+
 }
--- src/main/java/edu/ncsu/csc/itrust2/models/Vaccination.java
+++ src/main/java/edu/ncsu/csc/itrust2/models/Vaccination.java
@@ -1,5 +1,44 @@
 package edu.ncsu.csc.itrust2.models;
 
-public class Vaccination {
-    
+import com.google.gson.annotations.JsonAdapter;
+import edu.ncsu.csc.itrust2.adapters.LocalDateAdapter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "vaccination")
+public class Vaccination extends DomainObject{
+
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @NotNull
+    @ManyToOne private Vaccine vaccine;
+
+    @Setter
+    @NotNull @Basic
+    @Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
+    @JsonAdapter(LocalDateAdapter.class)
+    private LocalDate vaccinationDay;
+
+    @Setter
+    @NotNull @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "patient_id", columnDefinition = "varchar(100)")
+    private User patient;
+
+    @Setter
+    @NotNull
+    @Length(max = 500) private String comments;
+
 }
