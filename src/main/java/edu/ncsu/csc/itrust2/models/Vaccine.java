c661f5aad2cf048f62c0bcfa401d031976737f7a
Wonjae Choi <choi@wonj.me>
2023-11-26 15:24:41 +0900

    feat: Add models for vaccination

---
+++ src/main/java/edu/ncsu/csc/itrust2/models/Vaccine.java
@@ -0,0 +1,49 @@
+package edu.ncsu.csc.itrust2.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Represents a vaccine in the system.
+ */
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "vaccines")
+public class Vaccine extends DomainObject {
+
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @NotEmpty
+    @Size(max = 250)
+    private String name;
+
+    @Setter
+    @NotEmpty
+    @Size(max = 10)
+    private String abbreviation;
+
+    @Setter
+    @NotEmpty
+    @Pattern(regexp = "90\\d{3}")
+    private String cptCode;
+
+    @Setter
+    @Size(max = 500)
+    private String comments;
+
+    // Additional methods and logic as required
+}
